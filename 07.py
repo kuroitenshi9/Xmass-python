3df502163a190221995669aec782d87a6dae1648
Ewa B <ewabielawska@daum.net>
2019-12-13 20:16:12 +0100

    07.py

---
+++ 07.py
@@ -0,0 +1,28 @@
+"""
+Dzień 8
+
+Dla tablicy liczb całkowitych, chcemy, 
+aby liczby całkowite parzyste poprzedzały nieparzyste w tablicy
+
+Przykładowa tablica:
+numbers = [7,7,4,0,9,8,2,4,1,9]
+Optymalizacja
+Operate on the array in-place, with a constant amount of extra space. 
+The answer should scale linearly in time with respect to the size of the array.
+"""
+
+def sort_odds2even(lista):
+    even = []
+    odd = []
+    for i in lista:
+        if (i % 2) == 0:
+            even.append(i)
+        else:
+            odd.append(i)
+    sorted_list = even + odd
+    return print(sorted_list)
+
+if __name__ == '__main__':
+    numbers = [7,7,4,0,9,8,2,4,1,9]
+    sort_odds2even(numbers)
+
