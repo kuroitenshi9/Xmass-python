37d615ba8d91cb97b2d270ac6f319ce8526fb1b3
Ewa B <ewabielawska@daum.net>
2019-12-13 20:26:10 +0100

    updated

--- 07.py
+++ 07.py
@@ -6,7 +6,8 @@ aby liczby całkowite parzyste poprzedzały nieparzyste w tablicy
 
 Przykładowa tablica:
 numbers = [7,7,4,0,9,8,2,4,1,9]
-Optymalizacja
+
+***Optymalizacja:
 Operate on the array in-place, with a constant amount of extra space. 
 The answer should scale linearly in time with respect to the size of the array.
 """
@@ -22,7 +23,9 @@ def sort_odds2even(lista):
     sorted_list = even + odd
     return print(sorted_list)
 
-if __name__ == '__main__':
+def main():
     numbers = [7,7,4,0,9,8,2,4,1,9]
     sort_odds2even(numbers)
 
+if __name__ == '__main__':
+    main()
